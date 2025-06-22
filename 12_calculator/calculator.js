842b7ae54fc71ab6e0c2f266e3b9670f5a701ab4
Charlie <clairecoppe98@gmail.com>
2025-06-22 13:19:19 +0200

    exercices done, all tests passed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0,);
 
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1,);
+
 };
 
-const factorial = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
+};
+
+const factorial = function(a) {
+	if (a === 0 || a === 1)
+    return 1;
+  for (var i = a - 1; i>=1; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
