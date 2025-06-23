d4885b7f97365bac45665401e8d801c2ee5ba07c
Charlie <clairecoppe98@gmail.com>
2025-06-23 12:14:34 +0200

    passed all the tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if ( a < 0 ){
+        return "OOPS";
+    }
+    else {
+        let fib = [0, 1];
+        for (let i = 2; i <= a; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[a];
+    }
 };
 
 // Do not edit below this line
